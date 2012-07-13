1521fc37c615eae57a10b08fa64457e2e0b94ade
Hynek Vychodil <vychodil.hynek@gmail.com>
2012-07-13 10:47:21 +0200

    Add sum_di implementation.

--- c_src/srdbe.c
+++ c_src/srdbe.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <erl_nif.h>
+#include <assert.h>
 
 #ifdef USE_NATIVE
 #  define SWAP32(x) (x)
@@ -11,109 +12,344 @@
 #endif
 
 #define I32NULLVAL (0x80000000)
+#define I64NULLVAL (0x8000000000000000)
+
+static ErlNifResourceType *srdbe_rw_bin_type = NULL;
+
+typedef struct {
+    size_t size;
+    void *data;
+} rw_bin;
+
+static struct {
+    ERL_NIF_TERM ok;
+    ERL_NIF_TERM not_null;
+    ERL_NIF_TERM default_null;
+} srdbe_atoms;
+
+    static void
+release_rw_bin(ErlNifEnv* env, void* obj)
+{
+    enif_free(((rw_bin *)obj)->data);
+    return;
+}
 
 static int 
 load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
 {
-//  puts("Loading nif\r");
+  //  puts("Loading nif\r");
+
+  ErlNifResourceType *rt = enif_open_resource_type(env, "srdbe",
+      "rw_bin", release_rw_bin, ERL_NIF_RT_CREATE, NULL);
+
+  if (rt == NULL)
+    return -1;
+
+  assert(srdbe_rw_bin_type == NULL);
+  srdbe_rw_bin_type = rt;
+
+  srdbe_atoms.ok           = enif_make_atom(env, "ok");
+  srdbe_atoms.not_null     = enif_make_atom(env, "not_null");
+  srdbe_atoms.default_null = enif_make_atom(env, "default_null");
+
   return 0;
 }
 
-static int 
+  static int
 upgrade(ErlNifEnv* env, void** priv_data, void** old_priv_data, ERL_NIF_TERM load_info)
 {
-//  puts("Upgrading nif\r");
+  //  puts("Upgrading nif\r");
+
+  ErlNifResourceType *rt = enif_open_resource_type(env, "srdbe",
+      "rw_bin", release_rw_bin, ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER, NULL);
+
+  if (rt == NULL)
+    return -1;
+
+  srdbe_rw_bin_type = rt;
+
   return 0;
 }
 
-static void
+  static void
 unload(ErlNifEnv* env, void* priv_data)
 {
-//  puts("Unloading nif\r");
+  //  puts("Unloading nif\r");
   return;
 }
 
-static ERL_NIF_TERM test(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+  static ERL_NIF_TERM
+test(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    return enif_make_int(env, sizeof(int));
+  return enif_make_int(env, sizeof(size_t));
 }
 
-static ERL_NIF_TERM lift1_ii(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+    static ERL_NIF_TERM
+lift1_ii(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-  ErlNifBinary index, data, result;
+    ErlNifBinary index, data, result;
 
-  if(!enif_inspect_binary(env, argv[0], &index)) {
-    return enif_make_badarg(env);
-  }
-
-  if(!enif_inspect_binary(env, argv[1], &data)) {
-    return enif_make_badarg(env);
-  }
-
-  enif_alloc_binary(index.size, &result);
-
-  {
-    int32_t *indexdata = (int32_t *)index.data;
-    int32_t *datadata = (int32_t *)data.data;
-    int32_t *resultdata = (int32_t *)result.data;
-    unsigned int indexsize = index.size >> 2;
-    unsigned int datasize = data.size >> 2;
-    unsigned int pos;
-    unsigned int i;
-    for(i=0; i<indexsize; i++) {
-      pos = SWAP32(indexdata[i]);
-      if(pos == I32NULLVAL) {
-        resultdata[i] = SWAP32(I32NULLVAL);
-      }
-      else if(!(pos<datasize)) {
-        enif_release_binary(&result);
+    if(!(
+                enif_inspect_binary(env, argv[0], &index) &&
+                enif_inspect_binary(env, argv[1], &data) &&
+                enif_alloc_binary(index.size, &result)
+        ))
         return enif_make_badarg(env);
-      }
-      else {
-        resultdata[i] = datadata[pos];
-      };
-    };
-  }
 
-  return enif_make_binary(env, &result);
+    {
+        int32_t *indexdata = (int32_t *)index.data;
+        int32_t *datadata = (int32_t *)data.data;
+        int32_t *resultdata = (int32_t *)result.data;
+        unsigned int indexsize = index.size / sizeof(int32_t);
+        unsigned int datasize = data.size / sizeof(int32_t);
+        unsigned int pos;
+        unsigned int i;
+        for(i=0; i<indexsize; i++) {
+            pos = SWAP32(indexdata[i]);
+            if(pos == I32NULLVAL) {
+                resultdata[i] = SWAP32(I32NULLVAL);
+            }
+            else if(!(pos<datasize)) {
+                enif_release_binary(&result);
+                return enif_make_badarg(env);
+            }
+            else {
+                resultdata[i] = datadata[pos];
+            };
+        };
+    }
+
+    return enif_make_binary(env, &result);
 }
 
-static ERL_NIF_TERM lift1_is(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+    static ERL_NIF_TERM
+lift1_is(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-  ErlNifBinary index, data, result;
+    ErlNifBinary index, data, result;
 
-  if(!enif_inspect_binary(env, argv[0], &index)) {
-    return enif_make_badarg(env);
-  }
+    if(!(
+                enif_inspect_binary(env, argv[0], &index) &&
+                enif_inspect_binary(env, argv[1], &data) &&
+                enif_alloc_binary((index.size+31) / 32, &result)
+        ))
+        return enif_make_badarg(env);
 
-  if(!enif_inspect_binary(env, argv[1], &data)) {
-    return enif_make_badarg(env);
-  }
+    memset(result.data, 0, result.size);
 
-  enif_alloc_binary((index.size+31) >> 5, &result);
-  memset(result.data, 0, result.size);
-
-  {
-    int32_t *indexdata = (int32_t *)index.data;
-    char *datadata = (char *)data.data;
-    char *resultdata = (char *)result.data;
-    unsigned int indexsize = index.size >> 2;
-    unsigned int datasize = data.size << 3;
-    unsigned int pos;
-    unsigned int i;
-    for(i=0; i<indexsize; i++) {
-      pos = SWAP32(indexdata[i]);
-      if(pos != I32NULLVAL) {
-        if(!(pos<datasize)) {
-          enif_release_binary(&result);
-          return enif_make_badarg(env);
+    {
+        int32_t *indexdata = (int32_t *)index.data;
+        char *datadata = (char *)data.data;
+        char *resultdata = (char *)result.data;
+        unsigned int indexsize = index.size / sizeof(int32_t);
+        unsigned int datasize = data.size * 8;
+        unsigned int pos;
+        unsigned int i;
+        for(i=0; i<indexsize; i++) {
+            pos = SWAP32(indexdata[i]);
+            if(pos != I32NULLVAL) {
+                if(!(pos<datasize)) {
+                    enif_release_binary(&result);
+                    return enif_make_badarg(env);
+                };
+                resultdata[i >> 3] |= (((datadata[pos >> 3] << (pos & 7)) & (1<<7)) >> (i & 7));
+            };
         };
-        resultdata[i >> 3] |= (((datadata[pos >> 3] << (pos & 7)) & (1<<7)) >> (i & 7));
-      };
-    };
-  }
+    }
 
-  return enif_make_binary(env, &result);
+    return enif_make_binary(env, &result);
+}
+
+    static ERL_NIF_TERM
+has_null_i(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    ErlNifBinary data;
+    ERL_NIF_TERM bin, list = argv[0];
+
+    while (!enif_is_empty_list(env, list)) {
+        if(!(
+                    enif_get_list_cell(env, list, &bin, &list) &&
+                    enif_inspect_binary(env, bin, &data)
+            ))
+            return enif_make_badarg(env);
+
+        {
+            int32_t *datadata = (int32_t *)data.data;
+            size_t datasize = data.size / sizeof(int32_t);
+            size_t i;
+            for(i=0; i<datasize; i++)
+                if(datadata[i] == SWAP32(I32NULLVAL))
+                    return srdbe_atoms.default_null;
+        }
+    }
+
+    return srdbe_atoms.not_null;
+}
+
+    static ERL_NIF_TERM
+has_null_d(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    ErlNifBinary data;
+    ERL_NIF_TERM bin, list = argv[0];
+
+    while (!enif_is_empty_list(env, list)) {
+        if(!(
+                    enif_get_list_cell(env, list, &bin, &list) &&
+                    enif_inspect_binary(env, bin, &data)
+            ))
+            return enif_make_badarg(env);
+
+        {
+            int64_t *datadata = (int64_t *)data.data;
+            size_t datasize = data.size / sizeof(int64_t);
+            size_t i;
+            for(i=0; i<datasize; i++)
+                if(datadata[i] == SWAP64(I64NULLVAL))
+                    return srdbe_atoms.default_null;
+        }
+    }
+
+    return srdbe_atoms.not_null;
+}
+
+    static ERL_NIF_TERM
+create_rw_bin_i(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    int size;
+    rw_bin *bin;
+    ERL_NIF_TERM term;
+
+    if(!enif_get_int(env, argv[0], &size))
+        return enif_make_badarg(env);
+
+    bin = enif_alloc_resource(srdbe_rw_bin_type, sizeof(rw_bin));
+    if(bin == NULL)
+        return enif_make_badarg(env);
+
+    bin->size = size * sizeof(int32_t);
+    bin->data = enif_alloc(bin->size);
+
+    {
+        int32_t *data = (int32_t *)(bin->data);
+        int i;
+        for(i=0; i<size; i++)
+            data[i] = SWAP32(I32NULLVAL);
+    }
+
+    term = enif_make_resource(env, bin);
+    enif_release_resource(bin);
+    return term;
+}
+
+    static ERL_NIF_TERM
+create_rw_bin_d(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    int size;
+    rw_bin *bin;
+    ERL_NIF_TERM term;
+
+    if(!enif_get_int(env, argv[0], &size))
+        return enif_make_badarg(env);
+
+    bin = enif_alloc_resource(srdbe_rw_bin_type, sizeof(rw_bin));
+    if(bin == NULL)
+        return enif_make_badarg(env);
+
+    bin->size = size * sizeof(int64_t);
+    bin->data = enif_alloc(bin->size);
+
+    {
+        int64_t *data = (int64_t *)(bin->data);
+        int i;
+        for(i=0; i<size; i++)
+            data[i] = SWAP64(I64NULLVAL);
+    }
+
+    term = enif_make_resource(env, bin);
+    enif_release_resource(bin);
+    return term;
+}
+
+    static ERL_NIF_TERM
+make_bin(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    rw_bin *bin;
+    int chunk_size;
+    ERL_NIF_TERM list;
+
+    if(!(
+                enif_get_resource(env, argv[0], srdbe_rw_bin_type, (void **) &bin) &&
+                enif_get_int(env, argv[1], &chunk_size) &&
+                (chunk_size > 0)
+        ))
+        return enif_make_badarg(env);
+
+    {
+        size_t size = bin->size;
+        char *ptr = (char *)(bin->data) + size;
+        size_t reminder = size % chunk_size;
+
+        if(reminder > 0) {
+            ptr -= reminder;
+            list = enif_make_list1(env,
+                    enif_make_resource_binary(env, bin, ptr, reminder));
+        }
+        else {
+            list = enif_make_list(env, 0);
+        }
+
+        ptr -= chunk_size;
+        while((void *)ptr >= bin->data) {
+            list = enif_make_list_cell(
+                    env,
+                    enif_make_resource_binary(env, bin, ptr, chunk_size),
+                    list);
+            ptr -= chunk_size;
+        }
+    }
+    return list;
+}
+
+    static ERL_NIF_TERM
+sum_di(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    rw_bin *bin;
+    ErlNifBinary data, index, set;
+
+    if(!(
+                enif_get_resource(env, argv[0], srdbe_rw_bin_type, (void **) &bin) &&
+                enif_inspect_binary(env, argv[1], &data) &&
+                enif_inspect_binary(env, argv[2], &index) &&
+                enif_inspect_binary(env, argv[3], &set) &&
+                (data.size == 2*index.size) &&
+                (set.size*8 >= index.size/4)
+        ))
+        return enif_make_badarg(env);
+
+    {
+        int64_t *datadata = (int64_t *)data.data;
+        int64_t *bindata = (int64_t *)(bin->data);
+        int32_t pos, *indexdata = (int32_t *)index.data;
+        size_t binsize = (bin->size)/sizeof(int64_t);
+        char *setdata = (char *)set.data;
+        size_t i, max = data.size/sizeof(int64_t);
+
+        for(i=0; i < max; i++) {
+            if(
+                    (setdata[i>>3] & ((1<<7) >> (i & 7))) &&
+                    (datadata[i] != SWAP64(I64NULLVAL))
+              ) {
+                pos = SWAP32(indexdata[i]);
+                if (!(pos < binsize)) {
+                    return enif_make_badarg(env);
+                }
+                bindata[pos] = (bindata[pos] == SWAP64(I64NULLVAL))
+                    ? datadata[i]
+                    : SWAP64(SWAP64(bindata[pos])+SWAP64(datadata[i]));
+            }
+        }
+    }
+
+    return srdbe_atoms.ok;
 }
 
 static ErlNifFunc nif_funcs[] =
@@ -121,6 +357,12 @@ static ErlNifFunc nif_funcs[] =
     {"test", 0, test},
     {"lift1_ii", 2, lift1_ii},
     {"lift1_is", 2, lift1_is},
+    {"has_null_i", 1, has_null_i},
+    {"has_null_d", 1, has_null_d},
+    {"create_rw_bin_i", 1, create_rw_bin_i},
+    {"create_rw_bin_d", 1, create_rw_bin_d},
+    {"make_bin", 2, make_bin},
+    {"sum_di_", 4, sum_di},
 };
 
 ERL_NIF_INIT(srdbe, nif_funcs, load, NULL, upgrade, unload)
--- src/srdbe.erl
+++ src/srdbe.erl
@@ -1,8 +1,102 @@
+%%%-------------------------------------------------------------------
+%%% @author Hynek Vychodil
+%%% @copyright 2012 Hynek Vychodil
+%%% @doc Low level srdbe engine
+%%%
+%%% srdbe works with binary streams. Module expects all input data has
+%%% proper size.
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+
 -module(srdbe).
 
--export([init/0, test/0, lift_ii/2, lift_is/2]).
+-export([test/0,
+         lift_ii/2,
+         lift_is/2,
+         sum_di/5]).
+
+-compile(export_all).
 
 -on_load(init/0).
+
+-define(CHUNK_SIZE, 65536).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+-type stream() :: [binary()].
+
+-type nullity() :: 'not_null' | 'default_null'.
+
+-spec test() -> any().
+
+test() ->
+  exit(nif_not_loaded).
+
+%% @doc Lift Data using Index
+%%
+%% The function makes new {@link stream().}. Result data are from `Data'
+%% and are stored in place defined by index `Idx'. This function transforms
+%% integer (i32) data indexed by integer index.
+%%
+%% The operation can be symbolically written as:
+%% ```
+%% for(i=0; i<Idx.size; i++)
+%%   Result[i] = Data[Idx[i]];
+%% '''
+%% @end
+
+-spec lift_ii( Idx, Data ) -> stream() when
+  Idx :: stream(),
+  Data :: stream().
+
+lift_ii(IDX, [D]) ->
+  [ lift1_ii(I, D) || I<-IDX ].
+
+%% @doc Lift Data using Index
+%%
+%% The function works in the same manner as {@link lift_ii/2.} but
+%% transforms binary (boolean) data indexed by integer index.
+%%
+%% @see lift_ii/2.
+%% @end
+
+-spec lift_is( Idx, Data ) -> stream() when
+  Idx :: stream(),
+  Data :: stream().
+
+lift_is(IDX, [D]) ->
+  [ lift1_is(I, D) || I<-IDX ].
+
+%% @doc Sum data using index
+%%
+%% The function sums `Data' into stream indexed by `Idx' filtered by `Set'.
+%% Function makes stream tuple with type `Type'. Function requires `Size' of
+%% output stream.
+%%
+%% @see lift_ii/2.
+%% @end
+
+-spec sum_di(Type, Data, Idx, Set, Size) ->
+  {'stream', {Type, nullity()}, Result} when
+  Type :: any(),
+  Data :: stream(),
+  Idx :: stream(),
+  Set :: stream(),
+  Size :: integer(),
+  Result :: stream().
+
+sum_di(Type, Data, Idx, Set, Size) ->
+  Accu = create_rw_bin_d(Size),
+  sum_di(Accu, Data, Idx, Set),
+  Result = make_bin(Accu, ?CHUNK_SIZE*8),
+  {stream, {Type, has_null_d(Result)}, Result}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
 
 so_name() -> "srdbe".
 
@@ -21,19 +115,31 @@ init() ->
   end,
   erlang:load_nif(SoName, 1).
 
-test() ->
-  exit(nif_not_loaded).
-
-
-lift_ii(IDX, [D]) ->
-  [ lift1_ii(I, D) || I<-IDX ].
-
 lift1_ii(_, _) ->
   exit(nif_not_loaded).
-
-lift_is(IDX, [D]) ->
-  [ lift1_is(I, D) || I<-IDX ].
 
 lift1_is(_, _) ->
   exit(nif_not_loaded).
 
+create_rw_bin_i(_) ->
+  exit(nif_not_loaded).
+
+create_rw_bin_d(_) ->
+  exit(nif_not_loaded).
+
+sum_di(Accu, [HF|TF], [HI|TI], [HS|TS]) ->
+  sum_di_(Accu, HF, HI, HS),
+  sum_di(Accu, TF, TI, TS);
+sum_di(Accu, [], [], []) -> Accu.
+
+sum_di_(_, _, _, _) ->
+  exit(nif_not_loaded).
+
+make_bin(_, _) ->
+  exit(nif_not_loaded).
+
+has_null_i(_) ->
+  exit(nif_not_loaded).
+
+has_null_d(_) ->
+  exit(nif_not_loaded).
